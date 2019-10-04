543dc7924d989bf9bb36fa3959b155acfb7f6058
Wellington Souza <48191187+hayukizinho@users.noreply.github.com>
2019-10-04 00:50:04 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,124 @@
+import discord
+import asyncio
+import time
+
+client = discord.Client()
+msg_id = None
+msg_user = None
+COR = 0xF7FE2E
+players = {}
+
+messages = joined = 0
+
+
+@client.event
+async def on_ready():
+    print(client.user.name)
+    print("BOT ONLINE - Ola Mundoo!!")
+    print(client.user.id)
+    print('==---------------------==')
+
+@client.event
+async def on_member_join(member):
+    canal = client.get_channel("550864250298564610")
+    regras = client.get_channel("551627572144766978")
+    msg = "Bem vindo ao puteiro, {}, sou o BOT - {}".format(member.mention, client.user.name)
+    await client.send_message(canal, msg)
+
+@client.event
+async def on_member_remove(member):
+    canal = client.get_channel("550864250298564610")
+    msg = "Vá com beijos queridas - {}".format(member.mention)
+    await client.send_message(canal, msg)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!entrar'):
+        try:
+            channel = message.author.voice.voice_channel
+            await client.join_voice_channel(channel)
+        except discord.errors.InvalidArgument:
+            await client.send_message(message.channel, "O BOT ja esta em um canal de voz")
+        except Exception as error:
+            await client.send_message(message.channel, "Ocorreu um erro: ```{error}```".format(error=error))
+
+    if message.content.startswith('!sair'):
+        try:
+            mscleave = discord.Embed(
+                title="\n",
+                color=COR,
+                description="Sai do canal de voz e a musica parou!"
+            )
+            voice_client = client.voice_client_in(message.server)
+            await client.send_message(message.channel, embed=mscleave)
+            await voice_client.disconnect()
+        except AttributeError:
+            await client.send_message(message.channel, "O bot não esta em nenhum canal de voz.")
+        except Exception as Hugo:
+            await client.send_message(message.channel, "Ocorreu um erro: ```{haus}```".format(haus=Hugo))
+
+    if message.content.startswith('!play'):
+        try:
+            yt_url = message.content[6:]
+            if client.is_voice_connected(message.server):
+                try:
+                    voice = client.voice_client_in(message.server)
+                    players[message.server.id].stop()
+                    player = await voice.create_ytdl_player('ytsearch: {}'.format(yt_url))
+                    players[message.server.id] = player
+                    player.start()
+                    mscemb = discord.Embed(
+                        title="Música para tocar:",
+                        color=COR
+                    )
+                    mscemb.add_field(name="Nome:", value="`{}`".format(player.title))
+                    await client.send_message(message.channel, embed=mscemb)
+                except Exception as e:
+                    await client.send_message(message.server, "Erro: [{error}]".format(error=e))
+
+            if not client.is_voice_connected(message.server):
+                try:
+                    channel = message.author.voice.voice_channel
+                    voice = await client.join_voice_channel(channel)
+                    player = await voice.create_ytdl_player('ytsearch: {}'.format(yt_url))
+                    players[message.server.id] = player
+                    player.start()
+                    mscemb2 = discord.Embed(
+                        title="Música para tocar:",
+                        color=COR
+                    )
+                    mscemb2.add_field(name="Nome:", value="`{}`".format(player.title))
+                    await client.send_message(message.channel, embed=mscemb2)
+                except Exception as error:
+                    await client.send_message(message.channel, "Error: [{error}]".format(error=error))
+        except Exception as e:
+            await client.send_message(message.channel, "Erro: [{error}]".format(error=e))
+
+
+
+
+    if message.content.startswith('!pause'):
+        try:
+            mscpause = discord.Embed(
+                title="\n",
+                color=COR,
+                description="Musica pausada com sucesso!"
+            )
+            await client.send_message(message.channel, embed=mscpause)
+            players[message.server.id].pause()
+        except Exception as error:
+            await client.send_message(message.channel, "Erro: [{error}]".format(error=error))
+    if message.content.startswith('!resume'):
+        try:
+            mscresume = discord.Embed(
+                title="\n",
+                color=COR,
+                description="Musica pausada com sucesso!"
+            )
+            await client.send_message(message.channel, embed=mscresume)
+            players[message.server.id].resume()
+        except Exception as error:
+            await client.send_message(message.channel, "Error: [{error}]".format(error=error))
+
+
+client.run('NjI5NTIxMjgwNDcwODc2MTcw.XZa9cg.T03P5a1ad-QgyhvsxqbRHZ1Z6Cc')
